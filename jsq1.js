19170311a295c277e667e3b0bf32166ae6afe25f
yugal dhiman <yugaldhiman14@gmail.com>
2024-03-10 10:58:48 +0530

    add prompt

--- jsq1.js
+++ jsq1.js
@@ -1,10 +1,11 @@
-// const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();
 let age = prompt("Enter the your age");
 if (age > 10 && age < 20) {
   console.log("your age 20 and 10");
 } else {
   console.log("your age not 20 and 10");
 }
+console.log(age);
 
 // for (let i=1;i<=100;i++) {
 //     setTimeout(function(){
